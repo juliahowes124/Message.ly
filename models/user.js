3383b75f696acb85044a2ffd26e705a988214853
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-29 12:09:39 -0700

    implemented user model methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require('../config');
+
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+
 /** User of the site. */
 
 class User {
@@ -9,22 +17,54 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const resp = await db.query(`
+    INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+    VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+    RETURNING username, password, first_name, last_name, phone
+    `, [username, hashedPwd, first_name, last_name, phone])
+
+    return resp.rows[0]
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const resp = await db.query(`
+    SELECT password FROM users
+    WHERE username = $1
+    `, [username]);
+
+    if(resp.rows.length === 0) throw new NotFoundError();
+
+    const hashed_pwd = resp.rows[0].password;
+    const isValidCredentials = bcrypt.compare(password, hashed_pwd);
+    if(isValidCredentials) {
+      this.updateLoginTimestamp()
+    }
+    return isValidCredentials
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    await db.query(`
+    UPDATE users
+    SET last_login_at=CURRENT_TIMESTAMP
+    WHERE username = $1
+    `, [username])
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const resp = await db.query(`
+      SELECT username, first_name, last_name
+      FROM users
+    `)
+    return resp.rows;
   }
 
   /** Get: get user by username
@@ -37,6 +77,14 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const resp = await db.query(`
+    SELECT username, first_name, last_name, phone, join_at, last_login_at
+    FROM users
+    WHERE username = $1
+    `, [username])
+
+    if(resp.rows.length === 0) throw new NotFoundError();
+    return resp.rows[0];
   }
 
   /** Return messages from this user.
@@ -48,6 +96,20 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const resp = await db.query(`
+    SELECT m.id, m.body, m.sent_at, m.read_at, u.username, u.first_name, u.last_name, u.phone
+    FROM messages AS m
+    JOIN users AS u
+    ON u.username = m.to_username
+    WHERE m.from_username = $1
+    `, [username])
+
+    return resp.rows.map((row) => {
+      const {id, body, sent_at, read_at, username, first_name, last_name, phone} = row
+      return {id, body, sent_at, read_at, to_user: 
+              {username, first_name, last_name, phone}}
+    })
+
   }
 
   /** Return messages to this user.
@@ -59,6 +121,19 @@ class User {
    */
 
   static async messagesTo(username) {
+    const resp = await db.query(`
+    SELECT m.id, m.body, m.sent_at, m.read_at, u.username, u.first_name, u.last_name, u.phone
+    FROM messages AS m
+    JOIN users AS u
+    ON u.username = m.from_username
+    WHERE m.to_username = $1
+    `, [username])
+
+    return resp.rows.map((row) => {
+      const {id, body, sent_at, read_at, username, first_name, last_name, phone} = row
+      return {id, body, sent_at, read_at, from_user: 
+              {username, first_name, last_name, phone}}
+    })
   }
 }
 

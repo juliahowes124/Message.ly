aedb460e8b474a28f47b1819e6d3c4719308a5b4
Jon K <Johnakao@gmail.com>
2021-03-29 15:25:50 -0700

    Added some authentication routes.

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,25 @@
 
 const Router = require("express").Router;
 const router = new Router();
-
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config")
+const User = require("../models/user")
+const { UnauthorizedError } = require("../expressError")
 /** POST /login: {username, password} => {token} */
 
+router.post("/login", async function (req, res, next) {
+  let {username, password} = req.body;
+  let token = jwt.sign({ username }, SECRET_KEY);
+  if (await User.authenticate(username, password)) {
+    return res.json({ token });
+  } 
+  return next(new UnauthorizedError());
+});
 
-/** POST /register: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- */
+router.post("/register", async function (req, res, next) {
+  let { username } = await User.register(req.body);
+  let token = jwt.sign({ username }, SECRET_KEY)
+  return res.json({ token })
+})
 
 module.exports = router;
